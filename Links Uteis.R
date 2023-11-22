c58852f4db70af44a8331ba4f5cc39808d9018b3
FELIPPE LAZAR <felippelazarneto@gmail.com>
2023-11-22 09:57:50 -0300

    Adding Helpful Links

---
+++ Links Uteis.R
@@ -0,0 +1,9 @@
+
+# BBC Style - https://bbc.github.io/rcookbook/
+
+# Github Data-Science Course - https://github.com/datasciencelabs/2023/blob/main/04-r-basics.qmd
+
+# Statistica Inference PhD Level - https://mattblackwell.github.io/gov2002-book/01_intro.html
+
+# R Tutor.Ai - https://rtutor.ai/
+
