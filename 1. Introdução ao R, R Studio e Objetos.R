6b24282509cb702bb01f10d357b3bffbfe29eacd
FELIPPE LAZAR <felippelazarneto@gmail.com>
2023-11-22 09:16:13 -0300

    Update Aula 1

--- 1. Introdução ao R, R Studio e Objetos.R
+++ 1. Introdução ao R, R Studio e Objetos.R
@@ -3,3 +3,28 @@
 # Autor: Felippe Lazar Neto, 2023-2            #
 # ============================================================================ #
 
+# R é uma linguagem de programação derivada de uma outra linguagem chamada S
+# A base de uma linguagem de programação são objetos e funções. 
+# Objetos são unidades de informação criadas durante o ato de programar (a partir do sinal <-)
+# As funções funcionam como criadores ou modificadores ou executores de ações a partir de objetos
+# As funções possuem argumentos (informações necessárias para realizar determinada ação) - orientações de programar
+# As instruções para cada função podem ser acessadas com o '?' antes do nome da função
+
+# Unidades Basicas de Programação - Tipos de Objetos
+
+# Vetores são o objeto mais básico de R - são uma coletânea de componentes de mesmo tipo
+# criados a partir da função c()
+
+# 1.1 Vetores Numéricos
+
+# 1.2 Vetores de Caracteres (String)
+
+# 1.3 Vetores Booleanos (Verdadeiro ou Falso)
+
+# 1.4 Vetores de Fator (variável nominal de R - representação número/caracter)
+
+# 2.1 Matrizes são elementos em array com mais de uma dimensão, com componentes do mesmo tipo
+
+# 3.1 Listas são coletâneas, conjuntos de objetos, não necessariamente de mesmo tipo
+
+# 4.1 Dataframes são conjunto de objetos dispostos em colunas e linhas, não necessariamente do mesmo tipo
