b9929d65d24c9d876829924b0f52c29bceedfaef
FELIPPE LAZAR <felippelazarneto@gmail.com>
2023-11-30 17:50:56 -0300

    Aula 2 Raw

---
+++ 2. Operações Lógicas, IfElse, For While Loop, Apply.R
@@ -0,0 +1,19 @@
+# ============================================================================ #
+# 2. Operações Lógicas, If-Else, For Loops, While Loops, Funções e Apply       #
+# Autor: Felippe Lazar Neto, 2023-2                                            #
+# ============================================================================ #
+
+# O conhecimento da lógica de verdadeiro ou falso permite uma série de ações no R
+# Para realizar uma condição, é necessário saber os operadores lógicos que são utilizados no R
+
+# 2.1 Operadores Lógicos
+
+# 2.2 Condição If/Else
+
+# 2.3 For Looping
+
+# 2.4 Criação de Funções
+
+# 2.5 Funções do Grupo 'Apply' (sapply e lapply)
+
+
