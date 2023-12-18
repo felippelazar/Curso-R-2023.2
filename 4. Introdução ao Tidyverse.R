5189dab1270fc13a665b60c6b772bdb234e5ddf4
FELIPPE LAZAR <felippelazarneto@gmail.com>
2023-12-18 09:44:48 -0300

    Create 4. Introdução ao Tidyverse.R

---
+++ 4. Introdução ao Tidyverse.R
@@ -0,0 +1,23 @@
+# ============================================================================ #
+# 4. Introdução ao Tidyverse - Select, Filter, Mutate, Rename, Pivot, Join     #
+# Autor: Felippe Lazar Neto, 2023-2                                            #
+# ============================================================================ #
+
+# 1. Select
+
+
+# 2. Filter
+
+
+# 3. Mutate
+
+
+# 4. Rename
+
+
+# 5. Join
+
+
+# 6. Pivot Wider and Pivot Longer
+
+
