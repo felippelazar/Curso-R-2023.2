5a845f6c8af6a2ec6c3fcdd75f3233bb92b33d05
FELIPPE LAZAR <felippelazarneto@gmail.com>
2023-12-18 09:46:19 -0300

    Update 4. Introdução ao Tidyverse.R

--- 4. Introdução ao Tidyverse.R
+++ 4. Introdução ao Tidyverse.R
@@ -3,6 +3,16 @@
 # Autor: Felippe Lazar Neto, 2023-2                                            #
 # ============================================================================ #
 
+# Working Directory
+getwd()
+
+# Opening Packages
+library(tidyverse)
+
+# Loading data-frames
+dfb <- read.table(file = 'datasets/acmg/acmg_sample_csv_example.csv', sep = ',', header = TRUE,  # Header = Se a 1L são os nomes das colunas
+                      stringsAsFactors = FALSE, na.strings = c('')) # StringAsFactors = argumento para definir todos strings (character) como fator
+
 # 1. Select
 
 
